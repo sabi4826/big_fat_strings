bf0ff74842300af915cf2285da9b183a03a7a999
Sabine <sabi4826@stud.kea.dk>
2022-08-30 13:56:50 +0200

    forslag til start js

--- big_strings.js
+++ big_strings.js
@@ -2,8 +2,9 @@
 
 document.addEventListener("DOMContentLoaded", start);
 
-let input = "";
+let input;
 let output;
+let selectValue;
 
 function start() {
   console.log("start func loaded");
@@ -14,8 +15,29 @@ function start() {
 function getInput() {
   console.log("getInput func loaded");
 
+  // get text from input field:
   input = document.querySelector("#theInput").value;
-  console.log("Input is:", input);
+  console.log("Input is:", typeof input, input);
+
+  // variable for value i drop down:
+  selectValue = document.querySelector("#dropdown").value;
+  console.log(selectValue);
+
+  formatString();
 }
 
+function formatString() {
+  console.log("formatString func loaded");
+
+  if (selectValue === "1") {
+    let allToLowerCase = input.toLowerCase;
+    console.log(allToLowerCase);
+    let firstLetter = allToLowerCase.substring(0, 1).toUpperCase;
+    let endOfWord = allToLowerCase.substring(1);
+    output = `${firstLetter}${endOfWord}`;
+    console.log(output);
+  } else if (selectValue === 4) {
+    // "*".repeat (.length)
+  }
+}
 // if-sætn med mulighederne, der får hver deres funktion? Til sidst en skriv output ind-func?
